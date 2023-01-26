1f4e29e114344dedc561b1cf6574bacca13d624e
Vitaly Kozadayev <vitkoz@gmail.com>
2023-01-26 21:59:54 +0200

    Adding the rest of the pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get 'about-us', to: 'home#about', as: 'about'
+  get 'services', to: 'home#services', as: 'services'
+  get 'gallery', to: 'home#gallery', as: 'gallery'
+  get 'reviews', to: 'home#reviews', as: 'reviews'
+  get 'contact-us', to: 'home#contacts', as: 'contacts'
+
   root "home#home"
 end
